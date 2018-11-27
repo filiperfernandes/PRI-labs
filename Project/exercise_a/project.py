c27c42fa7463d48daff3a2d489f81150f1d87f09
andremendes95 <andre.filipe.m.mendes@gmail.com>
2018-11-27 18:23:15 +0000

    update aline a

--- Project/exercise_a/project.py
+++ Project/exercise_a/project.py
@@ -8,25 +8,24 @@ import sys
 
 def index():
     csv.field_size_limit(sys.maxsize)
-    schema = Schema(id=NUMERIC(stored=True), manifest_id=TEXT(stored=True), party=TEXT(stored=True), content=TEXT)
+    schema = Schema(manifest_id=TEXT(stored=True), party=TEXT(stored=True), content=TEXT)
     ix = create_in("dir", schema)
     with open("en_docs_clean.csv", 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         writer = ix.writer()
-        i = 0
         for row in csv_reader:
-            i = i + 1
-            writer.add_document(id=i, manifest_id=row['manifesto_id'], party=row['party'], content=row['text'])
+            writer.add_document(manifest_id=row['manifesto_id'], party=row['party'], content=row['text'])
         writer.commit()
     ix = open_dir("dir")
     with ix.searcher() as searcher:
-        qu = QueryParser("content", ix.schema)
-        q = qu.parse("United")
+        qu = QueryParser("content", ix.schema, group=OrGroup)
+        q = qu.parse("diabetes veteran")
         results = searcher.search(q, sortedby="manifest_id", limit=None)
         for r in results:
             print(r)
-    print(results.scored_length(), 'manifestos')
-    return results
+
+        print(results.scored_length())
+        return results
 
 
 def statistics():
