e4b82e1846ec4f4e31bf53ce18ced1299e556f4e
andremendes95 <andre.filipe.m.mendes@gmail.com>
2018-11-28 16:18:16 +0000

    alinea a)

--- Project/exercise_a/project.py
+++ Project/exercise_a/project.py
@@ -19,17 +19,29 @@ def index():
     ix = open_dir("dir")
     with ix.searcher() as searcher:
         qu = QueryParser("content", ix.schema, group=OrGroup)
-        q = qu.parse("diabetes veteran")
+        q = qu.parse("veteran")
         results = searcher.search(q, sortedby="manifest_id", limit=None)
+        l_id = []
         for r in results:
             print(r)
-
-        print(results.scored_length())
+            l_id.append(r["manifest_id"])
+        list_manifest = remove(l_id)
+        print("\n", len(list_manifest), "manifests")
+        #print(results.scored_length(),)
         return results
+
 
 
 def statistics():
     results = index()
+
+
+def remove(duplicate):
+    final_list = []
+    for num in duplicate:
+        if num not in final_list:
+            final_list.append(num)
+    return final_list
 
 
 if __name__ == "__main__":
