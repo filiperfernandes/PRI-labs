74cf685fc0ec76be6d5af4dd69a99b6a10c7d9e0
andremendes95 <andre.filipe.m.mendes@gmail.com>
2018-11-29 23:27:42 +0000

    update

--- Project/exercise_a/project.py
+++ Project/exercise_a/project.py
@@ -20,50 +20,40 @@ def manifests(word):
             writer.add_document(linha=i, manifest_id=row['manifesto_id'], party=row['party'], content=row['text'])
             i = i + 1
         writer.commit()
+    s_words = word.split(",")
     ix = open_dir("dir")
     with ix.searcher() as searcher:
         qu = QueryParser("content", ix.schema, group=OrGroup)
-        q = qu.parse(word)
-        results = searcher.search(q, sortedby="manifest_id", limit=None)
-        m = ""
-        p = ""
-        nr_manifestos = 0
-        dic = {}
-        man = []
-        for r in results:
-            manifesto = r["manifest_id"]
-            party = r["party"]
-            print(manifesto)
-            if party not in dic:
-                dic[party] = 0
-            if manifesto not in man:
-                man.append(manifesto)
-                dic[party] += 1
-        print(dic)
+        i = 0
+        while i < len(s_words):
+            dic = {}
+            man = []
+            q = qu.parse(s_words[i])
+            results = searcher.search(q, sortedby="manifest_id", limit=None)
+            print("\nkeyword: ", s_words[i])
+            for r in results:
+                manifesto = r["manifest_id"]
+                party = r["party"]
+                if party not in dic:
+                    dic[party] = 0
+                if manifesto not in man:
+                    man.append(manifesto)
+                    dic[party] += 1
+            i = i + 1
+            print(dic)
+    return results
 
 
 def keyword(word):
-    csv.field_size_limit(sys.maxsize)
-    schema = Schema(linha=NUMERIC(stored=True), manifest_id=TEXT(stored=True), party=TEXT(stored=True),
-                    content=TEXT(stored=True))
-    ix = create_in("dir", schema)
-    with open("en_docs_clean.csv", 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        writer = ix.writer()
-        i = 2
-        for row in csv_reader:
-            writer.add_document(linha=i, manifest_id=row['manifesto_id'], party=row['party'], content=row['text'])
-            i = i + 1
-        writer.commit()
+    ix = open_dir("dir")
     s_words = word.split(",")
-    l = len(s_words)
     with ix.searcher() as searcher:
         qu = QueryParser("content", ix.schema, group=OrGroup)
         i = 0
         while i < len(s_words):
             q = qu.parse(s_words[i])
             results = searcher.search(q, sortedby="manifest_id", limit=None)
-            print(s_words[i])
+            print("\nkeyword: ", s_words[i])
             d = {}
             for r in results:
                 raw = r["content"]
@@ -83,7 +73,7 @@ def keyword(word):
 
 def statistics():
     word = sys.argv[1]
-    manifests(word)
+    res = manifests(word)
     keyword(word)
 
 
