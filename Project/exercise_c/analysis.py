cf38982a9462097dfce4ef5e9166a9f3f6d67db6
Filipe Ramalho Fernandes <filipe.ramalhofernandes@gmail.com>
2018-12-05 15:11:43 +0000

    refactor code

--- Project/exercise_c/analysis.py
+++ Project/exercise_c/analysis.py
@@ -1,19 +1,21 @@
 from collections import Counter
 from collections import defaultdict
 
-import spacy, sys, csv
+import spacy
 import argparse
+import sys
+import csv
 
 # Load English tokenizer, tagger, parser, NER and word vectors
+MAX = 1000000
+FILE_PATH = "../pri_project_data/en_docs_clean.csv"
 nlp = spacy.load('en_core_web_sm')
 csv.field_size_limit(sys.maxsize)
 party_list = []
 text_list = []
 text_per_party_list = []
 available_party_list = []
-doc_dict={}
-MAX = 1000000
-
+doc_dict = {}
 party_dict = {}
 
 
@@ -21,9 +23,15 @@ def get_parser():
     parser = argparse.ArgumentParser(description="""PRI statistical analysis tool""")
     parser.add_argument('-f', '--file', dest='file', help="""receive file from stdin [default: no]""",
                         action="store")
-    parser.add_argument('-g', '--globally', help="""Globally""",
+    parser.add_argument('-l', '--language', help="""Specify language?""",
                         action="store_true")
-    parser.add_argument('-p', '--party', help="""Party""",
+    parser.add_argument('-p', '--party', help="""What are the most mentioned entities for each party?""",
+                        action="store_true")
+    parser.add_argument('-g', '--globally', help="""What are the most mentioned entities globally?""",
+                        action="store_true")
+    parser.add_argument('-t', '--times', help="""How many times does any given party mention other parties?""",
+                        action="store_true")
+    parser.add_argument('-m', '--mention', help="""Which party is mentioned more times by the other parties?""",
                         action="store_true")
     return parser
 
@@ -45,7 +53,7 @@ def get_entity_list(list_of_entities):
 
 def get_most_named_entity_per_party():
 
-    with open("../pri_project_data/en_docs_clean.csv", 'r') as f:
+    with open(FILE_PATH, 'r') as f:
         # blocksize = 947778
         # text = f.read(blocksize)
         csv_reader = csv.DictReader(f)
@@ -84,13 +92,13 @@ def get_most_named_entity_per_party():
             res1 = get_entity_list(doc1)
             res2 = get_entity_list(doc2)
             final_res = res1 + res2
-            testDict = defaultdict(int)
+            test_dict = defaultdict(int)
 
             for key, val in final_res:
-                testDict[key] += val
+                test_dict[key] += val
 
             # RES yelds most named entities for each party
-            res = testDict.items()
+            res = test_dict.items()
             print(party + ":")
             print("Top entities mentioned:")
             print(res)
@@ -108,7 +116,7 @@ def get_most_named_entity_per_party():
 def get_most_named_entity_globally():
     text_str = ""
     final_res = []
-    with open("../pri_project_data/en_docs_clean.csv", 'r') as f:
+    with open(FILE_PATH, 'r') as f:
         csv_reader = csv.DictReader(f)
         for row in csv_reader:
             party_list.append(row['party'])
@@ -121,13 +129,13 @@ def get_most_named_entity_globally():
                 final_res += get_entity_list(doc)
                 text_str = ""
 
-        testDict = defaultdict(int)
+        test_dict = defaultdict(int)
 
         for key, val in final_res:
-            testDict[key] += val
+            test_dict[key] += val
 
         # RES yelds most named entities for each party
-        res = testDict.items()
+        res = test_dict.items()
         print("Top entities mentioned:")
         print(res)
 
@@ -144,28 +152,117 @@ def count_entities(list_of_entities, entity_list):
     return entity_count
 
 
+def most_mentioned_party_per_party():
+
+    with open(FILE_PATH, 'r') as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            party_list.append(row['party'])
+            text_list.append(row['text'])
+        count = 0
+
+        # Populate Party List
+        get_party_list(party_list)
+        # Make dictionary with all texts per party
+        for party in party_list:
+            for cParty in available_party_list:
+                if party == cParty and party is not None and party != "None":
+                    party_dict.update({party: str(party_dict.get(party)) + " " + str(count)})
+            count = count + 1
+
+        for party in available_party_list:
+            text = ""
+            current_text_list = party_dict.get(party).split()
+            for iid in current_text_list:
+                if iid is None or iid == 'None':
+                    continue
+                else:
+                    text += text_list[int(iid)]
+                    doc_dict.update({party: text})
+
+        # Get Most Named Entity Per Party
+
+        # TODO: save it in a structure!! or not
+        for party in available_party_list:
+            ola = doc_dict.get(party)
+            for counting in available_party_list:
+                if party == counting:
+                    continue
+                else:
+                    print(party + ' mentions ' + '"'+counting+'"' + ' (times):')
+                    print(ola.count(counting))
+
+
+def most_mentioned_by_all():
+    with open(FILE_PATH, 'r') as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            party_list.append(row['party'])
+            text_list.append(row['text'])
+        count = 0
+
+        # Populate Party List
+        get_party_list(party_list)
+        # Make dictionary with all texts per party
+        for party in party_list:
+            for cParty in available_party_list:
+                if party == cParty and party is not None and party != "None":
+                    party_dict.update({party: str(party_dict.get(party)) + " " + str(count)})
+            count = count + 1
+
+        for party in available_party_list:
+            text = ""
+            current_text_list = party_dict.get(party).split()
+            for iid in current_text_list:
+                if iid is None or iid == 'None':
+                    continue
+                else:
+                    text += text_list[int(iid)]
+                    doc_dict.update({party: text})
+
+        # Get Most Named Entity Per Party
+
+        # TODO: save it in a structure!! or not
+        party_mentioned = {}
+        for party in available_party_list:
+            ola = doc_dict.get(party)
+            for counting in available_party_list:
+                if party == counting:
+                    continue
+                else:
+                    counter = ola.count(counting)
+                    val = party_mentioned.get(counting)
+                    if val is None:
+                        party_mentioned.update({counting: counter})
+                    else:
+                        party_mentioned.update({counting: counter + val})
+        # print(party_mentioned)
+        max_party = max(party_mentioned, key=party_mentioned.get)
+        print(max_party + " is most mentioned in total of " + str(party_mentioned.get(max_party)) + " times")
+
+
 def main():
     parser = get_parser()
     args = vars(parser.parse_args())
     file = args['file']
     globally = args['globally']
     party = args['party']
+    mention = args['mention']
+    times = args['times']
 
     if file:
         print("Using file " + file)
-    if globally:
+    elif globally:
         get_most_named_entity_globally()
-    if party:
+    elif party:
         get_most_named_entity_per_party()
+    elif mention:
+        most_mentioned_party_per_party()
+    elif times:
+        most_mentioned_by_all()
     else:
-        print("Specify flag")
+        parser.print_help()
 
 
 if __name__ == "__main__":
     main()
-
-# doc = nlp(text)
-
-# Find named entities, phrases and concepts
-# for entity in doc.ents:
-#    print(entity.text, entity.label_)
